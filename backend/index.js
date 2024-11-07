c2f41e9c9b890269e54b6e0c05b21d727fe74491
shira0614 <shira>
2024-11-07 10:09:57 +0100

    cors fix

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const app = express()
 
 app.use(cors(
     {
-        origin: 'http://localhost:5173',
+        origin: 'https://omibreed-dep-be.onrender.com',
         credentials: true,
         methods: 'GET, POST, PUT, DELETE, OPTIONS',
     }
