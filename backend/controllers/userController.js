1256eca7461fef540541f08cc39583569f337e0f
shira0614 <shira>
2024-11-07 10:42:45 +0100

    fixed cookie settings

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -44,8 +44,8 @@ const authUser = (req, res) => {
 
                     res.cookie('token', token, {
                         httpOnly: true,
-                        secure: process.env.NODE_ENV === 'production',
-                        sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'lax',
+                        secure: true,
+                        sameSite: "None",
                         maxAge: 24 * 60 * 60 * 1000 // 24 ore
                     });
 
